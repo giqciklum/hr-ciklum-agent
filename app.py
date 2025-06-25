e572798f04651fca979701c70502f620af914239
giqciklum <giq@ciklum.com>
2025-06-25 18:28:22 +0000

    updates

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ MODEL_NAME = "gpt-4o"
 EMBEDDING_MODEL_NAME = "text-embedding-3-large"
 
 # ID de tu proyecto de Google Cloud
-GCP_PROJECT_ID = "619993214373"
+GCP_PROJECT_ID = "justifying-camera-44231-g4"
 
 if not API_KEY:
     logging.critical("FATAL: No se ha encontrado la 'OPENAI_API_KEY'.")
